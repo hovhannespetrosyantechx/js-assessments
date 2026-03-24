d7caa94157e38b495db20f07f67101653bf06c1a
hovhannespetrosyantechx <hovhannes.petrosyan.techx@gmail.com>
2026-03-24 18:37:32 +0400

    finished task-2

--- task-6/main.js
+++ task-6/main.js
@@ -2,21 +2,37 @@ const personalInfo = document.getElementById('info-form');
 const nameBtn = document.getElementById('name');
 const emailBtn = document.getElementById('email');
 
-window.addEventListener('DOMContentLoaded', function(){
-    const saved = this.localStorage.getItem('info');
-    if (saved){
-        const {name, email} = JSON.parse(saved);
+const noteForm = document.getElementById('note-form')
+const note = document.getElementById('temp-note');
+
+
+window.addEventListener('DOMContentLoaded', function () {
+    const savedInfo = localStorage.getItem('info');
+    const savedNote = sessionStorage.getItem('note');
+
+    if (savedInfo) {
+        const { name, email } = JSON.parse(savedInfo);
         nameBtn.value = name;
         emailBtn.value = email;
         console.log(nameBtn.value, emailBtn.value)
     }
+    if (savedNote) {
+        note.value = savedNote;
+    }
 })
-personalInfo.addEventListener('submit', function(e){
+
+personalInfo.addEventListener('submit', function (e) {
     e.preventDefault();
-    
+
     const formData = {
         name: nameBtn.value,
         email: emailBtn.value
     }
     localStorage.setItem('info', JSON.stringify(formData))
+})
+
+noteForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    sessionStorage.setItem('note', note.value)
 })
